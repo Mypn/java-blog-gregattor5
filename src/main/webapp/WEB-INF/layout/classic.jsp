6a837919b80b24f9ec8d26549219494c7b9ee877
Jarek <Jarek@Jarek-Komputer>
2016-09-04 16:00:32 +0200

    add bootstrap and spring tags

--- src/main/webapp/WEB-INF/layout/classic.jsp
+++ src/main/webapp/WEB-INF/layout/classic.jsp
@@ -1,27 +1,89 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-  <%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles" %>
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
 <!DOCTYPE html>
 <html>
 <head>
 
+
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+	integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+	crossorigin="anonymous">
 
 <!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+	integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+	crossorigin="anonymous">
+
+<script
+	src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.1/jquery.min.js"></script>
+
 
 <!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script
+	src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+	integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
+	crossorigin="anonymous"></script>
 
 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title><tiles:getAsString name="title"></tiles:getAsString></title>
 </head>
 <body>
-<tiles:insertAttribute name="body"/>
+
+	<div class="container">
+
+		<!-- Static navbar -->
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed"
+						data-toggle="collapse" data-target="#navbar" aria-expanded="false"
+						aria-controls="navbar">
+						<span class="sr-only">Toggle navigation</span> <span
+							class="icon-bar"></span> <span class="icon-bar"></span> <span
+							class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="<spring:url value="/"/>">JBA</a>
+				</div>
+				<div id="navbar" class="navbar-collapse collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href='<spring:url value="/"/>'> Home </a></li>
+						<li><a href="#">About</a></li>
+						<li><a href="#">Contact</a></li>
+						<li class="dropdown"><a href="#" class="dropdown-toggle"
+							data-toggle="dropdown" role="button" aria-haspopup="true"
+							aria-expanded="false">Dropdown <span class="caret"></span></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li role="separator" class="divider"></li>
+								<li class="dropdown-header">Nav header</li>
+								<li><a href="#">Separated link</a></li>
+								<li><a href="#">One more separated link</a></li>
+							</ul></li>
+					</ul>
+				</div>
+				<!--/.nav-collapse -->
+			</div>
+			<!--/.container-fluid -->
+		</nav>
+
+		<!-- Main component for a primary marketing message or call to action -->
+		
 
 
-<tiles:insertAttribute name= "footer"/>
+		<tiles:insertAttribute name="body" />
+
+
+		<tiles:insertAttribute name="footer" />
+
+
+	</div>
 </body>
 </html>
--- target/tmp/jsp/org/apache/jsp/WEB_002dINF/layout/classic_jsp.java
+++ target/tmp/jsp/org/apache/jsp/WEB_002dINF/layout/classic_jsp.java
@@ -11,10 +11,20 @@ public final class classic_jsp extends org.apache.jasper.runtime.HttpJspBase
 
   private static java.util.List<String> _jspx_dependants;
 
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_spring_url_value_nobody;
+
   private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
 
   public java.util.List<String> getDependants() {
     return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _jspx_tagPool_spring_url_value_nobody = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+    _jspx_tagPool_spring_url_value_nobody.release();
   }
 
   public void _jspService(HttpServletRequest request, HttpServletResponse response)
@@ -42,19 +52,34 @@ public final class classic_jsp extends org.apache.jasper.runtime.HttpJspBase
       _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
 
       out.write("\r\n");
-      out.write("  \r\n");
+      out.write("\r\n");
+      out.write("\r\n");
       out.write("<!DOCTYPE html>\r\n");
       out.write("<html>\r\n");
       out.write("<head>\r\n");
       out.write("\r\n");
+      out.write("\r\n");
       out.write("<!-- Latest compiled and minified CSS -->\r\n");
-      out.write("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">\r\n");
+      out.write("<link rel=\"stylesheet\"\r\n");
+      out.write("\thref=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\"\r\n");
+      out.write("\tintegrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\"\r\n");
+      out.write("\tcrossorigin=\"anonymous\">\r\n");
       out.write("\r\n");
       out.write("<!-- Optional theme -->\r\n");
-      out.write("<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">\r\n");
+      out.write("<link rel=\"stylesheet\"\r\n");
+      out.write("\thref=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\"\r\n");
+      out.write("\tintegrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\"\r\n");
+      out.write("\tcrossorigin=\"anonymous\">\r\n");
+      out.write("\r\n");
+      out.write("<script\r\n");
+      out.write("\tsrc=\"https://ajax.googleapis.com/ajax/libs/jquery/2.0.1/jquery.min.js\"></script>\r\n");
+      out.write("\r\n");
       out.write("\r\n");
       out.write("<!-- Latest compiled and minified JavaScript -->\r\n");
-      out.write("<script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\" integrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\" crossorigin=\"anonymous\"></script>\r\n");
+      out.write("<script\r\n");
+      out.write("\tsrc=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"\r\n");
+      out.write("\tintegrity=\"sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa\"\r\n");
+      out.write("\tcrossorigin=\"anonymous\"></script>\r\n");
       out.write("\r\n");
       out.write("\r\n");
       out.write("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\r\n");
@@ -64,14 +89,69 @@ public final class classic_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("</title>\r\n");
       out.write("</head>\r\n");
       out.write("<body>\r\n");
+      out.write("\r\n");
+      out.write("\t<div class=\"container\">\r\n");
+      out.write("\r\n");
+      out.write("\t\t<!-- Static navbar -->\r\n");
+      out.write("\t\t<nav class=\"navbar navbar-default\">\r\n");
+      out.write("\t\t\t<div class=\"container-fluid\">\r\n");
+      out.write("\t\t\t\t<div class=\"navbar-header\">\r\n");
+      out.write("\t\t\t\t\t<button type=\"button\" class=\"navbar-toggle collapsed\"\r\n");
+      out.write("\t\t\t\t\t\tdata-toggle=\"collapse\" data-target=\"#navbar\" aria-expanded=\"false\"\r\n");
+      out.write("\t\t\t\t\t\taria-controls=\"navbar\">\r\n");
+      out.write("\t\t\t\t\t\t<span class=\"sr-only\">Toggle navigation</span> <span\r\n");
+      out.write("\t\t\t\t\t\t\tclass=\"icon-bar\"></span> <span class=\"icon-bar\"></span> <span\r\n");
+      out.write("\t\t\t\t\t\t\tclass=\"icon-bar\"></span>\r\n");
+      out.write("\t\t\t\t\t</button>\r\n");
+      out.write("\t\t\t\t\t<a class=\"navbar-brand\" href=\"");
+      if (_jspx_meth_spring_url_0(_jspx_page_context))
+        return;
+      out.write("\">JBA</a>\r\n");
+      out.write("\t\t\t\t</div>\r\n");
+      out.write("\t\t\t\t<div id=\"navbar\" class=\"navbar-collapse collapse\">\r\n");
+      out.write("\t\t\t\t\t<ul class=\"nav navbar-nav\">\r\n");
+      out.write("\t\t\t\t\t\t<li class=\"active\"><a href='");
+      if (_jspx_meth_spring_url_1(_jspx_page_context))
+        return;
+      out.write("'> Home </a></li>\r\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">About</a></li>\r\n");
+      out.write("\t\t\t\t\t\t<li><a href=\"#\">Contact</a></li>\r\n");
+      out.write("\t\t\t\t\t\t<li class=\"dropdown\"><a href=\"#\" class=\"dropdown-toggle\"\r\n");
+      out.write("\t\t\t\t\t\t\tdata-toggle=\"dropdown\" role=\"button\" aria-haspopup=\"true\"\r\n");
+      out.write("\t\t\t\t\t\t\taria-expanded=\"false\">Dropdown <span class=\"caret\"></span></a>\r\n");
+      out.write("\t\t\t\t\t\t\t<ul class=\"dropdown-menu\">\r\n");
+      out.write("\t\t\t\t\t\t\t\t<li><a href=\"#\">Action</a></li>\r\n");
+      out.write("\t\t\t\t\t\t\t\t<li><a href=\"#\">Another action</a></li>\r\n");
+      out.write("\t\t\t\t\t\t\t\t<li><a href=\"#\">Something else here</a></li>\r\n");
+      out.write("\t\t\t\t\t\t\t\t<li role=\"separator\" class=\"divider\"></li>\r\n");
+      out.write("\t\t\t\t\t\t\t\t<li class=\"dropdown-header\">Nav header</li>\r\n");
+      out.write("\t\t\t\t\t\t\t\t<li><a href=\"#\">Separated link</a></li>\r\n");
+      out.write("\t\t\t\t\t\t\t\t<li><a href=\"#\">One more separated link</a></li>\r\n");
+      out.write("\t\t\t\t\t\t\t</ul></li>\r\n");
+      out.write("\t\t\t\t\t</ul>\r\n");
+      out.write("\t\t\t\t</div>\r\n");
+      out.write("\t\t\t\t<!--/.nav-collapse -->\r\n");
+      out.write("\t\t\t</div>\r\n");
+      out.write("\t\t\t<!--/.container-fluid -->\r\n");
+      out.write("\t\t</nav>\r\n");
+      out.write("\r\n");
+      out.write("\t\t<!-- Main component for a primary marketing message or call to action -->\r\n");
+      out.write("\t\t\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\t\t");
       if (_jspx_meth_tiles_insertAttribute_0(_jspx_page_context))
         return;
       out.write("\r\n");
       out.write("\r\n");
       out.write("\r\n");
+      out.write("\t\t");
       if (_jspx_meth_tiles_insertAttribute_1(_jspx_page_context))
         return;
       out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\t</div>\r\n");
       out.write("</body>\r\n");
       out.write("</html>");
     } catch (Throwable t) {
@@ -97,6 +177,58 @@ public final class classic_jsp extends org.apache.jasper.runtime.HttpJspBase
     _jspx_th_tiles_getAsString_0.setName("title");
     _jspx_th_tiles_getAsString_0.doTag();
     if (_jspx_resourceInjector != null) _jspx_resourceInjector.preDestroy(_jspx_th_tiles_getAsString_0);
+    return false;
+  }
+
+  private boolean _jspx_meth_spring_url_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  spring:url
+    org.springframework.web.servlet.tags.UrlTag _jspx_th_spring_url_0 = (org.springframework.web.servlet.tags.UrlTag) _jspx_tagPool_spring_url_value_nobody.get(org.springframework.web.servlet.tags.UrlTag.class);
+    _jspx_th_spring_url_0.setPageContext(_jspx_page_context);
+    _jspx_th_spring_url_0.setParent(null);
+    _jspx_th_spring_url_0.setValue("/");
+    int[] _jspx_push_body_count_spring_url_0 = new int[] { 0 };
+    try {
+      int _jspx_eval_spring_url_0 = _jspx_th_spring_url_0.doStartTag();
+      if (_jspx_th_spring_url_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_spring_url_0[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_spring_url_0.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_spring_url_0.doFinally();
+      _jspx_tagPool_spring_url_value_nobody.reuse(_jspx_th_spring_url_0);
+    }
+    return false;
+  }
+
+  private boolean _jspx_meth_spring_url_1(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  spring:url
+    org.springframework.web.servlet.tags.UrlTag _jspx_th_spring_url_1 = (org.springframework.web.servlet.tags.UrlTag) _jspx_tagPool_spring_url_value_nobody.get(org.springframework.web.servlet.tags.UrlTag.class);
+    _jspx_th_spring_url_1.setPageContext(_jspx_page_context);
+    _jspx_th_spring_url_1.setParent(null);
+    _jspx_th_spring_url_1.setValue("/");
+    int[] _jspx_push_body_count_spring_url_1 = new int[] { 0 };
+    try {
+      int _jspx_eval_spring_url_1 = _jspx_th_spring_url_1.doStartTag();
+      if (_jspx_th_spring_url_1.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+        return true;
+      }
+    } catch (Throwable _jspx_exception) {
+      while (_jspx_push_body_count_spring_url_1[0]-- > 0)
+        out = _jspx_page_context.popBody();
+      _jspx_th_spring_url_1.doCatch(_jspx_exception);
+    } finally {
+      _jspx_th_spring_url_1.doFinally();
+      _jspx_tagPool_spring_url_value_nobody.reuse(_jspx_th_spring_url_1);
+    }
     return false;
   }
 
